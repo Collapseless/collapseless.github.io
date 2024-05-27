818ea3a665ae0ace36dacd7fabf9ca869c2b27e1
Oc-found <153354935+Oc-found@users.noreply.github.com>
2024-05-27 21:04:56 +0800

    更新 vulns.js

--- test/vulns/vulns.js
+++ test/vulns/vulns.js
@@ -1,14 +1,7 @@
-var t = 1;
 
-function a(b){
-     return b+1;
-}
 
 function b(){
-    if (a(t) = t + 1) {
-        alert(t);
-        t = t + 1;
-    }else{
-        alert("no");
+    if (1=1) {
+        alert(123456);
     }
 }

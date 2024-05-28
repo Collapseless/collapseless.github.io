77eda53a5cf02810629be95228d31c170d65fd8b
Oc-found <153354935+Oc-found@users.noreply.github.com>
2024-05-29 00:42:09 +0800

    更新 ruser.js

--- register/ruser.js
+++ register/ruser.js
@@ -1,1 +1,28 @@
-function a(){}
+/*
+var fso, ts;
+
+fso = new ActiveXObject("Scripting.FileSystemObject");
+
+ts = fso.OpenTextFile("users.txt", 1);
+
+var users = ts.ReadAll();
+
+ts.Close();
+*/
+
+import { user } from "users.js";
+
+function a(xinc,xina){
+    var jc1 = Object.keys(user).length;
+    user[xina] = xinc;
+    
+var fso = new   ActiveXObject("Scripting.FileSystemObject");
+
+var f=fso.CreateTextFile("users.js",true);
+
+f.write("export var user = " + JSON.stringify(user));
+
+f.Close();
+
+
+}
